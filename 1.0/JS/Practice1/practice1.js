d637f3941a2a09f81da7bdc650f8b52e6e3bb517
LND78 <Namansoni78@outlook.com>
2025-04-21 06:17:35 +0530

    All Code Upload

---
+++ 1.0/JS/Practice1/practice1.js
@@ -0,0 +1,19 @@
+// console.log("Hello World"); //To print vale on console
+//Name = ("LND"); Wrong Method
+// let Name = ("LND"); // Right Way to set variable
+//Name = ("LND");
+//OR
+//const age = ("X") // IN const value is permanent and can't be redefined
+//console.log(Name);
+// console.log(Name + age)
+// typeof Name To Know datatype of variable
+// Data as object
+const student = {
+    fullName : "Papaya Pines",
+    Age : 295,
+    isPass : true
+    };
+    console.log(student)
+// console.log(student["fullName"])
+// OR
+console.log(student.fullName)
